2ff69be71372ac8dd00d628b20f6f36459f46e4a
高思达 <gsd13@mails.tsinghua.edu.cn>
2016-03-29 23:50:38 +0800

    课上涉及内容的部分修改

--- labcodes/lab4/kern/process/proc.h
+++ labcodes/lab4/kern/process/proc.h
@@ -11,7 +11,7 @@
 enum proc_state {
     PROC_UNINIT = 0,  // uninitialized
     PROC_SLEEPING,    // sleeping
-    PROC_RUNNABLE,    // runnable(maybe running)
+    PROC_RUNNABLE,    // runnable(maybe running) 就绪和运行分到了一起
     PROC_ZOMBIE,      // almost dead, and wait parent proc to reclaim his resource
 };
 
